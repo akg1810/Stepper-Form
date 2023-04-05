e6b94bc2111a3ee6870d578743b06a1d498f310c
Akansha <akansha29032001@gmail.com>
2023-04-06 00:41:09 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import HorizontalLinearStepper from './components/HorizontalLinearStepper';
+
+function App() {
+  return (
+    <div className="App">
+      <HorizontalLinearStepper />
+    </div>
+  );
+}
+
+export default App;
